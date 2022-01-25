39552f3cdf21ad4acc241023514e3d71eca3d8a5
Opilane TTHK <opilane@tthk.ee>
2022-01-25 09:41:30 +0200

    Add project files.

---
+++ zachet/module.py
@@ -0,0 +1,6 @@
+from random import *
+from tkinter import *
+def vsbot_():
+    p1=randint(1,3)
+    p2=randint(1,3)
+    
---
+++ zachet/zachet.py
@@ -0,0 +1,17 @@
+from random import *
+from module import *
+from tkinter import *
+root = Tk()
+root.geometry('500x200')  
+root.title('Камень,Ножницы,Бумага')
+vsbot=Button(root,text='Играть против бота',font='Arial 16',command=vsbot_)
+botvsbot=Button(root,text='Бот против бота',font='Arial 16')
+nazvanie=Label(root,text='Камень,Ножницы,Бумага',font='Arial 24')
+rezult=Button(root,text='Результаты игр',font='Arial 16')
+nazvanie.pack(side=TOP)
+vsbot.pack(side=TOP)
+botvsbot.pack(side=TOP)
+rezult.pack(side=TOP)
+
+
+root.mainloop()
